daca60494a42480c60513cfbaeab1b3c61565316
William <william-cribeiro@bct.ect.ufrn.br>
2019-09-25 00:27:34 -0300

    Delta L plot added

--- V-rep/Projeto 1/Seguidor_de_Caminho.m
+++ V-rep/Projeto 1/Seguidor_de_Caminho.m
@@ -29,8 +29,12 @@ clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
       
      %Polinomio interpolador
      [X, Y, Theta, a, b, Mat] = poli_cubic([-2,-2,0],[2,2,45],0.01);
+     %Ganhos do controlador
      k_theta = 0.6;
      k_l = 0.1;
+     
+     i = 1;
+     %Velocidade definida como constante
      v = 0.5;
      rd = 0.06;
      re = 0.06;
@@ -47,27 +51,29 @@ clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
          %calculo da menor distância do robô a curva
          [ponto_curva, Delta_l, lambda] = distance2curve(Mat(:,1:2),[position(1),position(2)],'linear');
 
-         Theta_robo = angulo_robo(3) + pi/2;                                % 1 -> 47:   OK
+         Theta_robo = angulo_robo(3) + pi/2;                               
 
          %raio de giro
          dx = a(2) +2*a(3)*lambda + 3*a(4)*(lambda^2);
          dy = b(2) + 2*b(3)*lambda + 3*b(4)*(lambda^2);
          d2x = 2*a(3) + 6*a(4)*lambda;
          d2y = 2*b(3) + 6*b(4)*lambda;
-         r = ((((dx^2)+(dy^2))^1.5)/((d2y*dx)-(d2x*dy)));               %ABS?????
+         r = ((((dx^2)+(dy^2))^1.5)/((d2y*dx)-(d2x*dy)));              
          k = (1/r);
          
          %delta theta
          theta_SF = atan((b(2) + 2*b(3)*(lambda) + 3*b(4)*(lambda^2))/(a(2) + 2*a(3)*(lambda) + 3*a(4)*(lambda^2)));%*180/pi;
          Delta_theta = Theta_robo - theta_SF;
          
+         %Garantir o sinal correto de Delta L 
          theta_posicao_robo = atan2(position(2),position(1)); %angulo de posicao do robo
          theta_ref = atan2(ponto_curva(2),ponto_curva(1)); %angulo da curva que está mais próxima ao robo
          
          if(theta_ref>theta_posicao_robo) %Sinal do delta L
              Delta_l = -Delta_l;
          end
-         
+         ErroL(i) = Delta_l;
+         i = i+1;
          %Controle
          u = -(k_theta*Delta_theta + (k_l*Delta_l*v*sin(Delta_theta)/Delta_theta));
          
@@ -80,13 +86,19 @@ clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
          
          vrep.simxSetJointTargetVelocity(clientID,left_Motor,we,vrep.simx_opmode_blocking);
          vrep.simxSetJointTargetVelocity(clientID,right_Motor,wd,vrep.simx_opmode_blocking);
-         
+                    
          if(lambda==1)
              break;
          end;
          
      end
      
+      %Plot do erro Delta l
+      figure(2)
+      plot(ErroL)
+      grid on;
+      ylabel('Delta L')
+      xlabel('Interação')
      [returnCode]=vrep.simxSetJointTargetVelocity(clientID,left_Motor,0,vrep.simx_opmode_blocking);
      [returnCode]=vrep.simxSetJointTargetVelocity(clientID,right_Motor,0,vrep.simx_opmode_blocking);
 
